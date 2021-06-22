de89b73caf8414840beb1cbe61c0b933269c8d93
Magdalini-P <82325735+Magdalini-P@users.noreply.github.com>
2021-06-22 17:34:38 +0300

    Update SQLQuery6.sql

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -6,7 +6,7 @@ for each row
 begin 
      if ((new.nfc_id, new.room_id) not in 
        (select nfc_id, room_id
-       from access inner join)
+       from access)
      ) then
       print 'Δεν είστε εγγεγραμμένος στην υπηρεσία που προσφέρει αυτό το δωμάτιο.'
      end if
